2688a785c9356c78c2de57a0dc4051f036379255
Jiwoo-O <jiwoo6538@gmail.com>
2024-03-17 22:51:15 +0900

    파비콘
    파비콘 -> 온실효과
    share.js -> 기본 링크 수정

--- js/share.js
+++ js/share.js
@@ -1,6 +1,6 @@
-const url = 'https://dev-dain.github.io/10-things-test';
-const title = '[이미지테스트] 나를 알아보는 10가지 질문 ';
-const hash = '%2310_things_test %233분이미지테스트 %23이미지테스트 ';
+const url = 'https://Jiwoo-O.github.io/carbon-neutrality-test/';
+const title = '탄소 중립 심리 테스트: carbon-neutrality-test ';
+const hash = '%carbon-neutrality-test %숙지고 %탄소중립심리테스트 ';
 
 const fb = () => {
   window.open('https://www.facebook.com/sharer/sharer.php?u='
